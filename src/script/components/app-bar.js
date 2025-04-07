2443705769d440ec6d16d7a2d56173eeb7db24f3
hpdrr <hapidr24@gmail.com>
2025-04-07 22:34:26 +0700

    Initial Commit; Added app-bar

---
+++ src/script/components/app-bar.js
@@ -0,0 +1,52 @@
+class AppBar extends HTMLElement {
+  _shadowRoot = null;
+  _style = null;
+
+  constructor() {
+    super();
+
+    this._shadowRoot = this.attachShadow({ mode: "open" });
+    this._style = document.createElement("style");
+  }
+
+  updateStyle() {
+    this._style.textContent = `
+      :host {
+        display: block;
+        width: 100%;
+        color: white;
+        box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.2);
+      }
+      div {
+          padding: 24px 20px;
+      }
+      .brand-name {
+          margin: 0;
+
+          font-size: 1.7em;
+      } 
+    `;
+  }
+
+  emptyContent() {
+    this._shadowRoot.innerHTML = "";
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.updateStyle();
+    this.emptyContent();
+
+    this._shadowRoot.appendChild(this._style);
+    this._shadowRoot.innerHTML += `
+    <div>
+      <h1 class="brand-name">Sport Club Finder>/h1>
+    </div>
+    `;
+  }
+}
+
+customElements.define("app-bar", AppBar);
